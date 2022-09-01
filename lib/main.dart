4ec6e864949b9176c0c40b58a385f030beb36dda
heback <heback@gmail.com>
2022-09-01 13:49:10 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+
+
+    final theme = FooderlichTheme.dark();
+
+    // TODO: Apply Home widget
+    // 3
+    return MaterialApp(
+      // TODO: Add theme
+      title: 'Fooderlich',
+      // 4
+      home: Scaffold(
+
+        appBar: AppBar(
+            title: Text('Fooderlich', style: theme.textTheme.headline6)
+        ),
+
+        body: Center(child: Text('Let\'s get cooking 👩‍🍳', style: theme.textTheme.headline1,)),
+      ),
+    );
+  }
+}
